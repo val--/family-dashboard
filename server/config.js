f17fec35914dd90a609e9275a51503aef6113563
val-- <khayvav@gmail.com>
2025-11-15 10:08:21 +0100

    Add missing files

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  calendarId: 'family03404702309255518191@group.calendar.google.com',
+  timezone: 'Europe/Paris',
+  maxEvents: 5,
+  credentialsPath: './credentials/service-account.json'
+};
+
