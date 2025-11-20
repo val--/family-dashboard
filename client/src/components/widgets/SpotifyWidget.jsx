8be64b032b5abb3b85f97530c0d7d9d6c4349358
val-- <valentin.guillaume@akeneo.com>
2025-11-20 20:03:58 +0100

    Better volume slider for spotify control

--- client/src/components/widgets/SpotifyWidget.jsx
+++ client/src/components/widgets/SpotifyWidget.jsx
@@ -26,6 +26,7 @@ function SpotifyWidget() {
   const [currentUser, setCurrentUser] = useState(null);
   const [playlistSortBy, setPlaylistSortBy] = useState('default'); // 'default' (ordre d'ajout), 'name'
   const [showSortMenu, setShowSortMenu] = useState(false);
+  const [showVolumeModal, setShowVolumeModal] = useState(false);
 
   // Vérifier l'authentification et récupérer le statut
   const fetchStatus = useCallback(async () => {
@@ -714,39 +715,72 @@ function SpotifyWidget() {
         </svg>
       </button>
 
-      {/* Slider de volume */}
+      {/* Icône de volume */}
       {activeDeviceId && (
-        <div className="spotify-widget-volume-slider">
-          <div className="spotify-widget-volume-icon">
-            {volume === 0 ? (
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
-              </svg>
-            ) : volume < 50 ? (
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/>
-              </svg>
-            ) : (
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
-              </svg>
-            )}
-          </div>
-          <div className="spotify-widget-volume-track">
-            <div 
-              className="spotify-widget-volume-fill" 
-              style={{ height: `${volume}%` }}
-            ></div>
-            <input
-              type="range"
-              min="0"
-              max="100"
-              value={volume}
-              onChange={(e) => handleVolumeChange(parseInt(e.target.value))}
-              className="spotify-widget-volume-input"
-              aria-label="Volume"
-              disabled={!activeDeviceId}
-            />
+        <button
+          className="spotify-widget-volume-icon-button"
+          onClick={() => setShowVolumeModal(true)}
+          aria-label="Ajuster le volume"
+        >
+          {volume === 0 ? (
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
+            </svg>
+          ) : volume < 50 ? (
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/>
+            </svg>
+          ) : (
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+            </svg>
+          )}
+        </button>
+      )}
+
+      {/* Modal de volume plein écran */}
+      {showVolumeModal && (
+        <div 
+          className="spotify-widget-volume-modal" 
+          onClick={() => setShowVolumeModal(false)}
+        >
+          <div className="spotify-widget-volume-modal-content" onClick={(e) => e.stopPropagation()}>
+            <button
+              className="spotify-widget-volume-modal-close"
+              onClick={() => setShowVolumeModal(false)}
+              aria-label="Fermer"
+            >
+              ×
+            </button>
+            <div className="spotify-widget-volume-modal-icon">
+              {volume === 0 ? (
+                <svg width="64" height="64" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
+                </svg>
+              ) : volume < 50 ? (
+                <svg width="64" height="64" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/>
+                </svg>
+              ) : (
+                <svg width="64" height="64" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+                </svg>
+              )}
+            </div>
+            <div className="spotify-widget-volume-modal-slider-container">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume}
+                onChange={(e) => handleVolumeChange(parseInt(e.target.value))}
+                className="spotify-widget-volume-modal-slider"
+                aria-label="Volume"
+                disabled={!activeDeviceId}
+                autoFocus
+              />
+              <div className="spotify-widget-volume-modal-value">{volume}%</div>
+            </div>
           </div>
         </div>
       )}
@@ -876,39 +910,72 @@ function SpotifyWidget() {
         </svg>
       </button>
 
-      {/* Slider de volume */}
+      {/* Icône de volume */}
       {activeDeviceId && (
-        <div className="spotify-widget-volume-slider">
-          <div className="spotify-widget-volume-icon">
-            {volume === 0 ? (
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
-              </svg>
-            ) : volume < 50 ? (
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/>
-              </svg>
-            ) : (
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
-              </svg>
-            )}
-          </div>
-          <div className="spotify-widget-volume-track">
-            <div 
-              className="spotify-widget-volume-fill" 
-              style={{ height: `${volume}%` }}
-            ></div>
-            <input
-              type="range"
-              min="0"
-              max="100"
-              value={volume}
-              onChange={(e) => handleVolumeChange(parseInt(e.target.value))}
-              className="spotify-widget-volume-input"
-              aria-label="Volume"
-              disabled={!activeDeviceId}
-            />
+        <button
+          className="spotify-widget-volume-icon-button"
+          onClick={() => setShowVolumeModal(true)}
+          aria-label="Ajuster le volume"
+        >
+          {volume === 0 ? (
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
+            </svg>
+          ) : volume < 50 ? (
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/>
+            </svg>
+          ) : (
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+            </svg>
+          )}
+        </button>
+      )}
+
+      {/* Modal de volume plein écran */}
+      {showVolumeModal && (
+        <div 
+          className="spotify-widget-volume-modal" 
+          onClick={() => setShowVolumeModal(false)}
+        >
+          <div className="spotify-widget-volume-modal-content" onClick={(e) => e.stopPropagation()}>
+            <button
+              className="spotify-widget-volume-modal-close"
+              onClick={() => setShowVolumeModal(false)}
+              aria-label="Fermer"
+            >
+              ×
+            </button>
+            <div className="spotify-widget-volume-modal-icon">
+              {volume === 0 ? (
+                <svg width="64" height="64" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
+                </svg>
+              ) : volume < 50 ? (
+                <svg width="64" height="64" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/>
+                </svg>
+              ) : (
+                <svg width="64" height="64" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+                </svg>
+              )}
+            </div>
+            <div className="spotify-widget-volume-modal-slider-container">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume}
+                onChange={(e) => handleVolumeChange(parseInt(e.target.value))}
+                className="spotify-widget-volume-modal-slider"
+                aria-label="Volume"
+                disabled={!activeDeviceId}
+                autoFocus
+              />
+              <div className="spotify-widget-volume-modal-value">{volume}%</div>
+            </div>
           </div>
         </div>
       )}
